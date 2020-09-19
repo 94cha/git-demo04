463f5b96e14b1655dc693d7ea821f8ae1685026d
xiaocha <623081550@qq.com>
2020-09-19 18:06:11 +0800

    demo04444

---
+++ src/main/java/com/cha/git/GitDemo021Application.java
@@ -0,0 +1,13 @@
+package com.cha.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemo021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemo021Application.class, args);
+	}
+
+}
